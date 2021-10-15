a66e7e5753c1fa477938c78c678f0592f570d9d2
Ujjwal Aakash <ujjwalaakash46ua@gmail.com>
2021-10-15 19:38:52 +0530

    adding: RestController

--- src/main/java/com/example/demo/controller/AddressbookController.java
+++ src/main/java/com/example/demo/controller/AddressbookController.java
@@ -1,5 +1,9 @@
 package com.example.demo.controller;
 
-public class AddressbookController {
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+public class AddressbookController {
+	
+	
 }
